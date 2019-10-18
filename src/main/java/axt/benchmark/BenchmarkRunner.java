bd5fc581df5d0d78efdad23ad250f7eb107a6e85
axt <axt@load.hu>
2019-10-18 03:38:52 +0200

    Simplified testcase, added GC and JAVA selection

--- src/main/java/axt/benchmark/BenchmarkRunner.java
+++ src/main/java/axt/benchmark/BenchmarkRunner.java
@@ -2,47 +2,48 @@ package axt.benchmark;
 
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class BenchmarkRunner {
 	public static void main(String[] args) {
-		Options opt = new OptionsBuilder()
-		        .include(".*")
-		        .jvm("/fast/shenandoah/build/linux-x86_64-server-fastdebug/images/jdk/bin/java")
-		        .jvmArgs("-Xms1024m", "-Xmx1024m")
-		        //.jvmArgsAppend("-XX:+UseConcMarkSweepGC", "-XX:+CMSConcurrentMTEnabled")
-		        //.jvmArgsAppend("-XX:+UseG1GC")
-		        //.jvmArgsAppend("-XX:+UnlockExperimentalVMOptions", "-XX:+UseZGC")
-		        .jvmArgsAppend("-XX:+UnlockExperimentalVMOptions", "-XX:+UseShenandoahGC")
+		String javaHome = System.getenv("JAVA_HOME");
+		String javaOpts = System.getenv("JAVA_OPTS");
+		String javaGC = System.getenv("JAVA_GC");
+		
+		ChainedOptionsBuilder optionsBuilder = new OptionsBuilder()
+				.include(".*")
+				.jvmArgs("-Xms1024m", "-Xmx1024m")
+				.forks(1);
+		
+		if (javaHome != null)
+			optionsBuilder = optionsBuilder.jvm(javaHome+"/bin/java");
 
-		        //.addProfiler(XProfiler.class)
-		        //.addProfiler(LinuxPerfAsmProfiler.class)
-		        //.addProfiler(LinuxPerfProfiler.class)
-		        //.addProfiler(LinuxPerfNormProfiler.class)
-		        //.addProfiler(SafepointsProfiler.class)
-		        //.addProfiler(HotspotClassloadingProfiler.class)
-		        //.addProfiler(HotspotCompilationProfiler.class)
-		        //.addProfiler(HotspotMemoryProfiler.class)
-		        //.addProfiler(HotspotRuntimeProfiler.class)
-		        //.addProfiler(HotspotThreadProfiler.class)
-		        //.addProfiler(ClassloaderProfiler.class)
-		        //.addProfiler(CompilerProfiler.class)
-		        //.addProfiler(GCProfiler.class)
-		        //.addProfiler(PausesProfiler.class)
-		        //.addProfiler(StackProfiler.class)
-		        //.addProfiler(JFRProfiler.class)
-		        
-		        //.jvmArgs("-Xmx256m", "-Xms256m", "-XX:+UnlockCommercialFeatures",
-		        //        "-Djmh.stack.profiles=" + destinationFolder,
-		        //        "-Djmh.executor=FJP",
-		        //        "-Djmh.fr.options=defaultrecording=true,settings=" + profile)
-		        //.result("/tmp/benchmarkResults.csv")
-		        //.resultFormat(ResultFormatType.CSV)
-		        .forks(1)
-		        .build();
-		 try {
-			new Runner(opt).run();
+		if (javaOpts != null)
+			optionsBuilder = optionsBuilder.jvm(javaOpts);
+
+		if (javaGC != null) {
+			switch (javaGC) {
+				case "CMS":
+					optionsBuilder = optionsBuilder.jvmArgsAppend("-XX:+UseConcMarkSweepGC", "-XX:+CMSConcurrentMTEnabled");
+					break;
+				case "G1":
+					optionsBuilder = optionsBuilder.jvmArgsAppend("-XX:+UseG1GC");
+					break;
+				case "Z":
+					optionsBuilder = optionsBuilder.jvmArgsAppend("-XX:+UnlockExperimentalVMOptions", "-XX:+UseZGC");
+					break;
+				case "Shenandoah":
+					optionsBuilder = optionsBuilder.jvmArgsAppend("-XX:+UnlockExperimentalVMOptions", "-XX:+UseShenandoahGC");
+					break;
+				default:
+					System.err.println("Unknown GC specified, possible choices: CMS, G1, Z, Shenandoah");
+					System.exit(1);
+					break;
+			}
+		}
+		try {
+			new Runner(optionsBuilder.build()).run();
 		} catch (RunnerException e) {
 			e.printStackTrace();
 		}		
--- src/main/java/axt/benchmark/TranscodeBenchmark.java
+++ src/main/java/axt/benchmark/TranscodeBenchmark.java
@@ -2,114 +2,61 @@ package axt.benchmark;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Timeout;
+import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
 
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Thread)
 public class TranscodeBenchmark {
+    byte[] src;
 
-	public static byte[] objectToByteArray(Serializable o) {
-		try {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			try(ObjectOutputStream oos = new ObjectOutputStream(new GZIPOutputStream(baos))) {
-				oos.writeObject(o);
-			}
-			baos.close();
-			return baos.toByteArray();
-		} catch(IOException ioe) {
-			throw new RuntimeException("Cannot convert object to bytearray: '" + o.getClass().getName() + "'", ioe);
-		} 
-	}
+    @Setup
+    public void setUp() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (GZIPOutputStream gos = new GZIPOutputStream(baos);
+            ObjectOutputStream oos = new ObjectOutputStream(gos)) {
+            oos.writeObject(new byte[65536]);
+        }
+        baos.close();
+        src = baos.toByteArray();
+    }
 
-	public Object byteArrayToObject(byte[] bs) {
-		Serializable obj;
-		try (ObjectInputStream ois = new ObjectInputStream(new GZIPInputStream(new ByteArrayInputStream(bs)))) {
-			obj = Serializable.class.cast(ois.readObject());
-		} catch (IOException | ClassNotFoundException ex) {
-			throw new RuntimeException("Exception happened while deserializing from a byte array", ex);
-		}
-		return obj;
-	}
-	
-	
-	public static class Model implements Serializable {
-		private static final long serialVersionUID = 1L;
-		private int[] ids;
-		public Model(int n) {
-			Random rd = new Random(1);
-			ids = new int[n];
-			for (int i=0; i<n; i++) {
-				for (int j=0; j < ids.length; j++) { 
-					if (j%2==0) {
-						ids[j] = rd.nextInt();
-					}
-				}
-			}
-		}
-	}
-	
-	@State(Scope.Benchmark)
-	public static class ExecutionPlan {		
-		public final int N = 500;
-		
-		public int size = 1000;
-		
-		Map<String, byte[]> data = new HashMap<>();
+    private Object test() throws Exception {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(src);
+            GZIPInputStream gis = new GZIPInputStream(bais);
+            ObjectInputStream ois = new ObjectInputStream(gis)) {
+            return ois.readObject();
+        }
+    }
+    
+    @Benchmark
+    @Threads(1)
+    public Object testSingleThread() throws Exception {
+    	return test();
+    }
 
-		@Setup(Level.Trial)
-		public void setUp() {
-			for (int i=0; i<N; i++) {
-				data.put(String.valueOf(i), objectToByteArray(new Model(N)));
-			}
-		}
-	}
-	
-	@Benchmark
-	@BenchmarkMode(Mode.AverageTime)
-	@OutputTimeUnit(TimeUnit.SECONDS)
-	@Warmup(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
-	@Measurement(iterations = 10, time = 10, timeUnit = TimeUnit.SECONDS)
-	@Timeout(time=60, timeUnit=TimeUnit.MINUTES)
-	public void benchmark(final ExecutionPlan e, final Blackhole b) {
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 16, 10000L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(e.N));
-
-		for (int i=0; i < e.N; i++) {
-			final Map<String, byte[]> data = e.data;
-			executor.submit(new Runnable() {
-				public void run() {
-					for (byte[] ba : data.values()) {
-						b.consume(byteArrayToObject(ba));
-					}
-				}
-			});
-		}
-		
-		executor.shutdown();
-		try { 
-			executor.awaitTermination(10, TimeUnit.SECONDS); 
-		} catch (InterruptedException ex) {}
-	}
+    @Benchmark
+    @Threads(Threads.MAX)
+    public Object testAllThreads() throws Exception {
+    	return test();
+    }
 }
